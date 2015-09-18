c0f16186e22c4872d3dc96b470fdeaea680c2626
tindo24 <tindo24@gmail.com>
2015-09-17 23:21:18 -0400

    added updatetableItem.

---
+++ Java/com/gildedrose/UpdatableItem.java
@@ -0,0 +1,26 @@
+package com.gildedrose;
+
+import com.gildedrose.Item;
+
+public class UpdatableItem extends Item {
+
+	protected final  int qualityDec = 1;
+
+	public UpdatableItem(String name, int sellIn, int quality) {
+		super(name, sellIn, quality);
+		// TODO Auto-generated constructor stub
+	}
+	public void update(){
+		if( this.sellIn <=0){
+			this.quality -= 2  * qualityDec;
+		}
+		else{
+			this.quality -= qualityDec;
+		}
+		this.sellIn--;
+		this.quality = Math.max(0, this.quality);
+
+	}
+
+
+}
