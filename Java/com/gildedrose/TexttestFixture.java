dbe7707a4d78dc420dd3ac60d3c241095a825f2e
ggerrein <ggerrein@gmail.com>
2015-09-18 00:10:17 -0400

    Fixed red stuff

--- Java/com/gildedrose/TexttestFixture.java
+++ Java/com/gildedrose/TexttestFixture.java
@@ -4,17 +4,17 @@ public class TexttestFixture {
     public static void main(String[] args) {
         System.out.println("OMGHAI!");
 
-        Item[] items = new Item[] {
-                new Item("+5 Dexterity Vest", 10, 20), //
-                new Item("Aged Brie", 2, 0), //
-                new Item("Elixir of the Mongoose", 5, 7), //
-                new Item("Sulfuras, Hand of Ragnaros", 0, 80), //
-                new Item("Sulfuras, Hand of Ragnaros", -1, 80),
-                new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
-                new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
-                new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
+        UpdatableItem[] items = new UpdatableItem[] {
+                new UpdatableItem("+5 Dexterity Vest", 10, 20), //
+                new AgedBrie("Aged Brie", 2, 0), //
+                new UpdatableItem("Elixir of the Mongoose", 5, 7), //
+                new Sulfuras("Sulfuras, Hand of Ragnaros", 0, 80), //
+                new Sulfuras("Sulfuras, Hand of Ragnaros", -1, 80),
+                new BackstagePass("Backstage passes to a TAFKAL80ETC concert", 15, 20),
+                new BackstagePass("Backstage passes to a TAFKAL80ETC concert", 10, 49),
+                new BackstagePass("Backstage passes to a TAFKAL80ETC concert", 5, 49),
                 // this conjured item does not work properly yet
-                new Item("Conjured Mana Cake", 3, 6) };
+                new UpdatableItem("Conjured Mana Cake", 3, 6) };
 
         GildedRose app = new GildedRose(items);
 
